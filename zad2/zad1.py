ab752bd78bc431459d8f0ecbcf8735e37e46e5e0
mczepcz <michal.czepczynski@gmail.com>
2020-01-05 22:23:25 +0100

    improvments

--- zad2/zad1.py
+++ zad2/zad1.py
@@ -1,20 +1,18 @@
 import os
 import shutil
 
-dir = os.path.join(".","zad2","resources","lab2","files")
+dir = os.path.join("resources", "lab2", "files")
 dirlist = os.listdir(dir)
 
 for filename in dirlist:
     filepath = dir + os.sep + filename
     with open(filepath, "r") as file:
-        for line,sentence in enumerate(file,1):
+        for line, sentence in enumerate(file, 1):
             newDir = ''
-            if(line == 1):
+            if line == 1:
                 newDir = sentence.split(" ")[1]
                 newDirPath = dir+os.sep+newDir
                 if not(os.path.exists(newDirPath)):
-                    os.mkdir(newDirPath, 7777)
-                shutil.copy(filepath,newDirPath+os.sep+filename)
+                    os.makedirs(newDirPath, 0o777)
+                shutil.copy(filepath, newDirPath+os.sep+filename)
                 break
-            
-    
